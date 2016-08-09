25b72794e1e0b3a09eee95600aad619f51309834
tuzi_li <bobotu@1122.moe>
2016-08-09 19:44:06 +0800

    测试...

--- src/main/clojure/blog/init.clj
+++ src/main/clojure/blog/init.clj
@@ -1,13 +1,14 @@
 (ns blog.init
   (:gen-class :implements [javax.servlet.ServletContextListener])
-  (:import (javax.servlet ServletContextEvent)
-           (cn.leancloud LeanEngine)))
+  (:import (cn.leancloud LeanEngine)
+           (com.avos.avoscloud AVOSCloud)))
 
 (defn -contextInitialized
   [this arg1]
-  (LeanEngine/initialize
+  (AVOSCloud/useAVCloudCN)
+  (AVOSCloud/initialize
     (System/getenv "LEANCLOUD_APP_ID")
     (System/getenv "LEANCLOUD_APP_KEY")
     (System/getenv "LEANCLOUD_APP_MASTER_KEY")))
 
-(defn -contextDestroyed [this arg1] )
+(defn -contextDestroyed [this arg1])
